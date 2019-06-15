af72c5f4ce1cea285e2e8fec4958c7c5fd0c2e71
Takuumi <satu_noone@hotmail.com>
2019-06-15 15:20:27 +0900

    写真をfirestoreから取得

--- src/app/sake-data/sake-data.component.ts
+++ src/app/sake-data/sake-data.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import {Location} from '@angular/common';
 import {Sake} from '../sake/sake';
 import {SakeService} from '../sake.service'
+import { AngularFireStorage } from 'angularfire2/storage';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -13,19 +15,20 @@ import {SakeService} from '../sake.service'
 export class SakeDataComponent implements OnInit {
   sakeid:string;
   sake:Sake;
-  constructor(private route:ActivatedRoute, private location:Location, private rsv:SakeService) { }
+  constructor(private route:ActivatedRoute, private location:Location, 
+              private rsv:SakeService,private storage: AngularFireStorage,) { }
 
-  // @@@ detailに移動
-  //profileUrl: Observable<string | null>;
+  profileUrl: Observable<string | null>;
 
-  // @@@ detailに移動
-   // const ref = this.storage.ref('sakebook_pics/main.png');
-   // this.profileUrl = ref.getDownloadURL();
 
 
   ngOnInit() {
     this.sakeid = this.route.snapshot.paramMap.get('id');
     this.sake = this.rsv.getSake(this.sakeid);
+
+    var filePath = 'sakebook_pics/' + 'pict' + this.sakeid.toString() + '.jpeg'
+    const ref = this.storage.ref(filePath);
+    this.profileUrl = ref.getDownloadURL();
   }
 
   backToList() {
--- src/app/sake-list/sake-list.component.ts
+++ src/app/sake-list/sake-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import {Sake} from '../sake/sake'
 import {SakeService} from '../sake.service'
 
