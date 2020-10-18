6eef94d8b0ac139d6be16085d50b4a3285f9c9ad
Takuumi <satu_noone@hotmail.com>
2020-10-18 09:15:16 +0900

    go112へ移行

--- gae/app.go
+++ gae/app.go
@@ -1,9 +1,44 @@
 package main
 
-import "net/http"
+// [START import]
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
-func init() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        // no op
-    })
+// [END import]
+// [START main_func]
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
 }
+
+// [END main_func]
+
+// [START indexHandler]
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
+
+// [END indexHandler]
